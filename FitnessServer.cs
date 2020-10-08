99cb16fd90ddc2fb0a5480b34588be859bdf50fd
Davy <dj.raitt@student.avans.nl>
2020-10-08 16:25:08 +0200

    Made a login screen.

--- FitnessServer.cs
+++ FitnessServer.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerMyFitnessApp
 {
     class FitnessServer
     {
+
+        private static TcpListener listener;
+       // private static List<FitnessServerClient> clients = new List<FitnessServerClient>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting server....");
-            
+            Console.WriteLine("Starting server and waiting for clients..");
+
+            listener = new TcpListener(IPAddress.Any, 15243);
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+
+            while (true)
+            {
+
+            }
+
         }
+
+        private static void OnConnect(IAsyncResult ar)
+        {
+            var tcpClient = listener.EndAcceptTcpClient(ar);
+            Console.WriteLine($"Client connected from {tcpClient.Client.RemoteEndPoint}");
+            //check if the client already excists
+            //clients.Add(new FitnessServerClient(tcpClient));
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+        }
+
+        // internal static void Disconnect(FitnessServerClient client)
+        // {
+        //     clients.Remove(client);
+        //     client.Disconnect();
+        //     Console.WriteLine("Client disconnected");
+        // }
+
+
     }
 }
